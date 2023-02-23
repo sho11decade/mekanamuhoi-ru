947ed2cea3f3784295076a48791a7f72a93e237b
sho11decade <109501994+sho11decade@users.noreply.github.com>
2023-02-23 14:59:16 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
-basic.forever(function () {
-	
-})
+function 右旋回 (speed: number) {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, speed)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, speed)
+}
+function 後退 (speed: number) {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, speed)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CW, speed)
+}
+function 前進 (speed: number) {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, speed)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CW, speed)
+}
+function ストップ () {
+    motor.motorStopAll()
+}
+function 左旋回 (speed: number) {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, speed)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CW, speed)
+}
